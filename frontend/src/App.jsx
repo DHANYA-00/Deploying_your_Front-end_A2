d22e92ad0036fc6dafe6199a896375c28f7f0580
Dhanya Lakshmi s s <dhanyalakshmi.s.s.06@gmail.com>
2025-03-04 10:53:11 +0530

    completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='head'>
+      <h1 className='C'>Contact Details</h1><hr></hr>
+      <h3>Name:</h3> DhanyaLakshmi S S
+      <h3>Mobile Number:</h3> 8610339182
+      <h3>Address:</h3>  velan nagar,Chennai-600701.
+      <h3>Email Address:</h3> dhanya_06@gmail.com
+    </div>
+    </>
+  )
+}
+
+export default App
